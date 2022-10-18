f993d3fd7814bdbb36938cc0aed63741e1d931a3
rzaye <rzaye@AYDIN_RZAYEV>
2022-10-18 14:05:58 +0400

    Add project files.

---
+++ MyArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
+
+public class MyList
+{
+    private int[] _list = new int[0];
+
+    private void FillListBy(int num, bool AtTheEnd, bool AtTheBeginning)
+    {
+        if(AtTheEnd == true)
+        {
+            Array.Resize(ref _list, 1);
+            _list[_list.Length - 1] = num;
+        }
+        else if(AtTheBeginning == true)
+        {
+
+        }
+    }
+}
