ea22943b69139e0408b14d38859519b45afbab8f
AydinRzayev <rzayevaydin01@gmail.com>
2022-10-25 12:37:09 +0400

    The Last Changes

--- MyArrayList/Program.cs
+++ MyArrayList/Program.cs
@@ -1,5 +1,10 @@
 ﻿
 
+
+using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
 namespace MyArrayList
 {
     internal class Program
@@ -12,7 +17,7 @@ namespace MyArrayList
             list.Add(3);
             list.Add(4);
             list.Add(5);
-            list.DeleteN(9, true);
+            list.ReverseList();
             list.Print();
 
         }
@@ -23,35 +28,157 @@ public class MyList
 {
     private int[] _list = new int[0];
 
-
-    /*for (int i = index; i < _list.Length - 1; i++)
+    public void Deletevalue()
     {
-        _list[i] = _list[i + 1];
+        /* for (int i = index; i < _list.Length - 1; i++)
+        {
+            _list[i] = _list[i + 1];
+
+        }
+        Array.Resize(ref _list, _list.Length - 1);
+         */
+
 
     }
-    Array.Resize(ref _list, _list.Length - 1);*/
+
+    public void Sort(bool descending)
+    {
+        Array.Sort(_list, 0, _list.Length);
+        if (descending)
+        {
+            _list.Reverse();
+        }
+    }
+
+    public void MaxValue(bool index)
+    {
+        int max = _list[0];
+        int index_value = 0;
+        for(int i = 0; i < _list.Length; i++)
+        {
+            if(_list[i] > max)
+            {
+                max = _list[i];
+                index_value = i;
+            }
+        }
+        if(index = true)
+        {
+            Console.WriteLine($"The index of element is {index_value}");
+        }
+        Console.WriteLine($"Maximum value is {max}");
+    }
+    public void MinValue(bool index)
+    {
+        int min = _list[0];
+        int index_value = 0;
+        for (int i = 0; i < _list.Length; i++)
+        {
+            if (_list[i] < min)
+            {
+                min = _list[i];
+                index_value = i;
+            }
+        }
+        if (index = true)
+        {
+            Console.WriteLine($"The index of element is {index_value}");
+        }
+        Console.WriteLine($"Maximum value is {min}");
+    }
+        
+    
+
+    public void ReverseList()
+    {
+        int [] array = new int[_list.Length];
+        for(int i = 0; i < _list.Length; i++)
+        {
+            array[i] = _list[_list.Length - i - 1];
+        }
+        _list = array;
+    }
+
+    public int GetElement(int index)
+    {
+        return _list[index];
+    }
+
+    public void ChangeElement(int index, int value)
+    {
+        try
+        {
+            _list[index] = value;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("There is not such an element. Use Add method.");
+        }
+    }
+
+    public void GetIndexByValue(int Value)
+    {
+        int index = 0;
+        int count = 0;
+        for (int i = 0; i < _list.Length; i++)
+        {
+            if (_list[i] == Value)
+            {
+                index = i;
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+        {
+            Console.WriteLine(-1);
+        }
+        else
+        {
+            Console.WriteLine(index);
+
+        }
+
+    }
+
+    public int Length()
+    {
+        return _list.Length;
+    }
+
+    public void DeleteBy(int begin, int num)
+    {
+        for (int i = begin; i < _list.Length - num; i++)
+        {
+            _list[i] = _list[i + num];
+        }
+        Array.Resize(ref _list, _list.Length - num);
+    }
+
+
+
     public void DeleteN(int num, bool beginning)
     {
-        if((beginning == false) && (num <= _list.Length))
+        if ((beginning == false) && (num <= _list.Length))
         {
             Array.Resize(ref _list, _list.Length - num);
         }
-        else if((beginning == true) && (num <= _list.Length))
+        else if ((beginning == true) && (num <= _list.Length))
         {
-            for(int i = 0; i < num; i++)
+            for (int i = 0; i < num; i++)
             {
-                 for (int k = 0 ; k < _list.Length - 1; k++)
-                 {
-                   _list[k] = _list[k + 1];
-                 }
+                for (int k = 0; k < _list.Length - 1; k++)
+                {
+                    _list[k] = _list[k + 1];
+                }
             }
             Array.Resize(ref _list, _list.Length - num);
         }
-       
-        
-        
-       
-        
+
+
+
+
+
     }
 
     public void Delete(int index)
